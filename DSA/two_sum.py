b0b00c0682349c56bd92acda405b25465341de11
Anjin-SAK <anjinkumar2129@gmail.com>
2026-04-05 20:03:14 +0530

    added explanation in comments

--- DSA/two_sum.py
+++ DSA/two_sum.py
@@ -3,7 +3,10 @@ class Solution(object):
         prevMap = {}
         for x, k in enumerate(nums):
             diff = target - k
-            if diff in prevMap:
+            if diff in prevMap:# check's if required number was seen before
+            # if yes we found two numbers that add up to target
+            # prevMap[diff] gives index of previous number
+            # x is current index
                 return [prevMap[diff], x]
             prevMap[k] = x
         return
--- DSA/valid_anagram.py
+++ DSA/valid_anagram.py
@@ -1,10 +1,15 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s) != len(t):
+        if len(s) != len(t):  # If lengths are different, they cannot be anagrams
+        # anagrams must use exactly the same characters
+        # the same number of times.
             return False
         group1 , group2 = {} , {}
         for x in range(0,len(s)):
-            group1[s[x]]= group1.get(s[x],0)+1
+            group1[s[x]]= group1.get(s[x],0)+1 # s[x] → current character
+            # group1.get(s[x], 0) → get current count of that character
+            # if not present, default = 0
+            # +1 → increment count
+            # So this line counts frequency of each character in string s
             group2[t[x]]= group2.get(t[x],0)+1
         return group1 == group2 
-        
